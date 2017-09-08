74bf516f407f4a4dd09d3cc6e1b2a61bd8970185
Aviral Saxena <avisaxena33@gmail.com>
2017-09-08 13:06:23 -0500

    jesus christ big update
    lol

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-var burger = 
-{
-    name: "burger",
-    count: 0
-};
-
-var taco = 
-{
-    name: "taco",
-    count: 0
-};
-
-var ice = 
-{
-    name: "ice",
-    count: 0
-};
-
-var foodCount = [burger,taco,ice];
+var foodCount = [];
 
 
 var express = require("express");
@@ -25,7 +7,7 @@ var socket = require("socket.io");
 
 var server = app.listen(3000, function()
 {
-    console.log("listenting on port 3000");
+    console.log("listening on port 3000");
 });
 
 app.use(express.static("public"));
@@ -35,7 +17,7 @@ var io = socket(server);
 io.on("connection", function(socket)
 {
     console.log("Made socket connection", socket.id);
-    io.sockets.emit("upvote", foodCount);
+    io.sockets.emit("upvote",   foodCount);
     socket.on("upvote", function(data)
 {
     for(var i = 0; i< foodCount.length; i++)
