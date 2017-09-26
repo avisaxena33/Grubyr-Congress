ff4c72f6a2d3ed41f41a62dcfaed8ade82569a7c
Aviral Saxena <avisaxena33@gmail.com>
2017-09-26 13:27:18 -0500

    lmao
    its the big dong boa update dis dik large

--- public/js/createDeal.js
+++ public/js/createDeal.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function()
     var food = document.getElementById("food");
     var location = document.getElementById("location");
     var deal = document.getElementById("deal");
-    var btn = document.getElementById("submit");
-
+    var img = document.getElementById("img");
+    
     btn.addEventListener("click", function()
     {
         var foodItem = 
@@ -17,9 +17,10 @@ document.addEventListener("DOMContentLoaded", function()
             name: food.value,
             location: location.value,
             deal: deal.value,
-            votes: 0
+            votes: 0,
+            image: img.value
         };
-        
+        console.log(img.value);
         socket.emit("addFood", foodItem);
                 
     });
